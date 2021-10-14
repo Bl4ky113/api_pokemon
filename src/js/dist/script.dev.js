41dfbd4406dee25434f43d67136d1fef130d5658
Bl4ky113 <martinstevenhernandezortiz@gmail.com>
2021-10-14 15:04:57 -0500

    Agregado Style base Pokedex y info pokemon

--- src/js/dist/script.dev.js
+++ src/js/dist/script.dev.js
@@ -4,19 +4,23 @@
 var API_DATA = {
   url: "https://pokeapi.co/api/v2/",
   search: {
-    pokemons: "pokemon/"
+    pokemons: "pokemon/",
+    species: "pokemon-species/"
   }
 };
-var search_data = "";
+var search_data = "crobat";
 var api_url = "".concat(API_DATA.url).concat(API_DATA.search.pokemons).concat(search_data);
-fetch(api_url).then(function (data) {
-  if (data.ok) {
-    return data.json();
-  } else {
-    throw new Error("Error al Conectar con la Api");
-  }
-}).then(function (pokemon) {
-  return console.log(pokemon);
-})["catch"](function (error) {
-  return console.error("Error: ".concat(error));
-});
+
+function getPokemonData(data_url) {
+  fetch(data_url).then(function (data) {
+    if (data.ok) {
+      return data.json();
+    } else {
+      throw new Error("Error al Conectar con la Api");
+    }
+  }).then(function (pokemon) {
+    return console.log(pokemon);
+  })["catch"](function (error) {
+    return console.error("Error: ".concat(error));
+  });
+}
--- src/js/script.js
+++ src/js/script.js
@@ -4,22 +4,27 @@ const API_DATA = {
   url: "https://pokeapi.co/api/v2/",
   search: {
     pokemons: "pokemon/",
-
+    species: "pokemon-species/"
   }
 }
 
-let search_data = ``;
+let search_data = `crobat`;
 
 let api_url = `${API_DATA.url}${API_DATA.search.pokemons}${search_data}`;
 
-
-fetch(api_url)
-  .then((data) => {
-    if (data.ok) {
-      return data.json();  
-    } else {
-      throw new Error("Error al Conectar con la Api");
-    }
-  })
-  .then ( pokemon => console.log(pokemon) )
-  .catch( error => console.error(`Error: ${error}`) );
+function getPokemonData (data_url) {
+  fetch(data_url)
+    .then((data) => {
+      if (data.ok) {
+        return data.json();  
+      } else {
+        throw new Error("Error al Conectar con la Api");
+      }
+    })
+    .then ( 
+      pokemon => console.log(pokemon)
+    )
+    .catch( 
+      error => console.error(`Error: ${error}`) 
+    );
+}
